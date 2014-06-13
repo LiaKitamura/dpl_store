21da2ee3d4ef6840b4fe2781e074eed702d01b82
Lia Kitamura <kit4mura@gmail.com>
2014-06-13 12:05:43 -0600

    haven't committed in a bit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  # public methods are actions
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   # The index action
   # This should list all of our products
@@ -10,7 +12,6 @@ class ProductsController < ApplicationController
   # This should find a particular product
   # show.html.erb
   def show
-    @product = Product.find(params[:id])
   end
 
   # new.html.erb
@@ -22,14 +23,35 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = 'Good Job! You did it.'
       redirect_to products_path
     else
+      flash[:alert] = 'Bad job! You failed.'
       render :new
     end
   end
 
+  def edit
+  end
+
+  # redirect or render
+  def update
+    if @product.update_attributes(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  # redirect or render
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
   private
 
+  #  private methods are methods. methods in private can't be accesed outside of this class
   # define a private method
   # use string parameters
 
@@ -37,4 +59,7 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:name)
   end
 
+  def find_product
+    @product = Product.find(params[:id])
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
-end
+
+  def show_notice_message
+    # return flash[:notice] if flash[:notice].present?
+    # return flash[:alert] if flash[:alert].present?
+
+    # the above is rewritten like so :
+
+    # [:array_of_symbols, :array_of_symbols].each do |some_variable|
+      # return flash[some_variable] if flash[some_variable].present?
+    unless flash.empty?
+      [:notice, :alert].each do |key|
+        return content_tag(:div, flash[key], class: "flash-#{key}") if flash[key].present?
+        end
+      end
+    end
+  end
